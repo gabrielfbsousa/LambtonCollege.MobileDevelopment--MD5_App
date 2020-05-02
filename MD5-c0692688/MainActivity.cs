66ca6eab4c16678a20e010e688ae7f771fac71c5
Gabriel Ferreira <gabriel.fbsousa.94@gmail.com>
2020-05-01 21:18:10 -0300

    Added the whole MD5 App

---
+++ MD5-c0692688/MainActivity.cs
@@ -0,0 +1,78 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+
+namespace MD5c0692688
+{
+    [Activity(Label = "MD5-c0692688", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        private EditText _entryEditText;
+        private TextView _origTextView;
+        private TextView _hashedTextView;
+
+        private string originalText;
+        private string hashedText;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            //Matching the view elements on code
+            _entryEditText = FindViewById<EditText>(Resource.Id.editText1);
+            _origTextView = FindViewById<TextView>(Resource.Id.textView5);
+            _hashedTextView = FindViewById<TextView>(Resource.Id.textView7);
+
+            //Creating the button click handler
+            Button button = FindViewById<Button>(Resource.Id.button1);
+            button.Click += delegate
+            {
+                //Method that will do the conversion
+                updateHash();
+                _hashedTextView.Text = hashedText;
+                _origTextView.Text = originalText;
+            };
+        }
+
+        public async void updateHash()
+        {
+            //Creating the url
+            string userTypedHash = _entryEditText.Text;
+            string url = "http://md5.jsontest.com/?text=" + userTypedHash;
+
+            //Getting the JSON from the URL
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            //Getting the response from the URL
+            HttpResponseMessage messageResponse = await httpClient.GetAsync(url);
+            if (messageResponse != null || messageResponse.IsSuccessStatusCode)
+            {
+                string content = await messageResponse.Content.ReadAsStringAsync();
+                JObject item = JObject.Parse(content);
+
+
+                _origTextView.Text = (string)item.GetValue("original");
+                _hashedTextView.Text = (string)item.GetValue("md5");
+
+            }
+            else
+            {
+                Console.Out.WriteLine("An error happened here!");
+            }
+
+
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,30 @@
+![College](https://img.shields.io/badge/College-Lambton_College-blue)
+![Language](https://img.shields.io/badge/Language-C%23_and_Xamarin-blueviolet)
+![IDE](https://img.shields.io/badge/IDE-Visual_Studio-blueviolet)
+
+
+# Mobile Development :: MD5 App
+Project developed and delivered for the course of Mobile Development, on third term of the Computer Software & Database Development program, at Lambton College of Technology and Applied Arts. All the programs below were written in C# with Xamarin
+
+#### Project Information
+- Project: ``MD5 App``
+&nbsp;
+
+- Course code: ``CSD3184``
+&nbsp;
+
+- Status: ``Completed``
+&nbsp;
+
+- Technology: ``C# and Xamarin``
+&nbsp;
+
+- Date: ``Dec 2018``
+&nbsp;
+
+## Project Detailing
+### Details
+The goal of this application is to receive a text input from the user and, with this, convert the text into a MD5 Hash code. To do this, MD5 needs to connect to a web service through HTTP Requests in an async way, sending the text message. The server will convert and return the MD5 into a JSON, that should be parsed and the shown to the costumer.
+
+--- 
+Made by Gabriel Ferreira :computer: [Find me at Linkedin!](https://www.linkedin.com/in/gabriel-f-sousa/)
